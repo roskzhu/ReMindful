938bb02d43940d7faa9c0e49804bff678b5ada75
stanleyylin <stanleylin82@gmail.com>
2024-01-27 05:29:12 -0500

    started navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Connect from './pages/connect/Connect';
 import MyMind from './pages/myMind/MyMind';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
+      <NavBar/>
       <Routes>
         <Route index element={<Home/>} />
         <Route path="/connect" element={<Connect/>} />
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import styled from '@emotion/styled'
+import { Label1, Label2 } from '../constants/Labels'
+import { Button1 } from '../constants/Buttons'
+import { Link, useNavigate } from 'react-router-dom'
+
+const NavBar = () => {
+  const navigate = useNavigate();
+  const [ hoveredTab, setHoveredTab] = useState(0);
+  return (
+    <NavBarContainer>
+      <NavBarSubContainer>
+        <Link to={"/"} className='link'>
+          <Label2 className='cursor: pointer;'>
+            Home
+          </Label2>
+        </Link>
+        <Link to={"/connect"} className='link'>
+          <Label2 className='cursor: pointer;'>
+            Connect
+          </Label2>
+        </Link>
+      </NavBarSubContainer>
+
+      <Label1 className="margin-left: 7rem; margin-right: 7rem; cursor: pointer;">
+          remindful.
+      </Label1>
+
+      <NavBarSubContainer>
+        <Button1 className='justify-self: end; cursor: pointer;'
+        onClick={() =>
+          navigate("/myMind")
+        }>
+          My Mind
+        </Button1>
+      </NavBarSubContainer>
+    </NavBarContainer>
+  )
+}
+
+const NavBarContainer = styled.div`
+  height: 6rem;
+  width: 70vw;
+  margin: 20px;
+  background-color: rgba(128, 128, 128, 0.3);
+  display: flex; 
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  border-radius: 15px;
+  left: calc((100vw - 70vw) / 2);
+  justify-self: center;
+  backdrop-filter: blur(5px);
+  z-index: 9999;
+  .hide {
+    display: none;
+  }
+  .link {
+    text-decoration: none;
+  }
+`
+
+
+const NavBarSubContainer = styled.div`
+  display: flex;
+  width: 40%;
+  align-items: center;
+  justify-content: space-evenly;
+  justify-self: start;
+
+
+`;
+
+export default NavBar
